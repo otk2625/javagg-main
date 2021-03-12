4ad0e14976c424e06ab13b83cc9ab5a7d082c3b0
otk2625 <xorud8531@gmail.com>
2021-03-12 17:53:58 +0900

    공동작업2

--- app/src/main/java/com/cos/javagg/fragment/LoginFragment.java
+++ app/src/main/java/com/cos/javagg/fragment/LoginFragment.java
@@ -1,9 +1,11 @@
 package com.cos.javagg.fragment;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,20 @@ import androidx.fragment.app.Fragment;
 import com.cos.javagg.R;
 
 public class LoginFragment extends Fragment {
+    private TextView tvForgetPassword, tvJoin;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login,container,false);
+
+        tvForgetPassword = view.findViewById(R.id.tv_forgetpassword);
+        tvJoin = view.findViewById(R.id.tv_join);
+
+        tvForgetPassword.setText(Html.fromHtml( "<font color=\"#2a7de2\"><u>비밀번호를 잊으셨나요</u></font>"));
+        tvJoin.setText(Html.fromHtml( "<font color=\"#2a7de2\"><u>회원가입하기</u></font>"));
+
+
 
         return view;
     }
--- app/src/main/java/com/cos/javagg/fragment/SearchFragment.java
+++ app/src/main/java/com/cos/javagg/fragment/SearchFragment.java
@@ -15,33 +15,41 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import com.cos.javagg.MainActivity;
 import com.cos.javagg.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class SearchFragment extends Fragment {
-
-    private DrawerLayout mDrawerLayout;
+public class SearchFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener
+{
+    //private DrawerLayout mDrawerLayout;
+    private DrawerLayout drawLayout;
+    private NavigationView navigationView;
     private ImageButton draw;
     private Vibrator vibrator;
     private ImageButton btn1;
+    private Context at;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search,container,false);
+        at = (MainActivity)container.getContext();
 
-        mDrawerLayout=view.findViewById(R.id.drawerLayout);
-        draw = view.findViewById(R.id.draw);
+        //mDrawerLayout=view.findViewById(R.id.drawerLayout);
+        //draw = view.findViewById(R.id.draw);
         vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
         btn1=view.findViewById(R.id.img_button);
 
-        drawL();    // 드로우레이아웃
+        InitializeLayout(view);
+        //drawL();    // 드로우레이아웃
         lolclick(); // 롤 이미지클릭
 
         return view;
@@ -56,9 +64,57 @@ public class SearchFragment extends Fragment {
         });
     }
 
-    public void drawL(){
-        draw.setOnClickListener(v -> {
-            mDrawerLayout.openDrawer(GravityCompat.START);
-        });
+//    public void drawL(){
+//        draw.setOnClickListener(v -> {
+//            mDrawerLayout.openDrawer(GravityCompat.START);
+//        });
+//    }
+
+    public void InitializeLayout(View view)
+    {
+        //toolBar를 통해 App Bar 생성
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        //App Bar의 좌측 영영에 Drawer를 Open 하기 위한 Incon 추가
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_glyph_solid_gamepad);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+        drawLayout = (DrawerLayout) view.findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) view.findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                ((AppCompatActivity)getActivity()),
+                drawLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        );
+
+        drawLayout.addDrawerListener(actionBarDrawerToggle);
+        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+
     }
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch(item.getItemId()) {
+                case R.id.account:
+                    Toast.makeText(at, "item1 clicked..", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.setting:
+                    Toast.makeText(at, "item2 clicked..", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.logout:
+                    Toast.makeText(at, "item3 clicked..", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
+            drawLayout.closeDrawer(GravityCompat.START);
+            return false;
+
+        }
+
+
 }
